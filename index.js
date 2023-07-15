4db454863b23c9308fd3f434a75aae00dbc1c8a0
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-07-15 15:52:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+// let saturday='roller-skate';
+// function saturdayFun(){
+// return saturday
+
+// }
+// saturdayFun()
+function saturdayFun(defaultActivity = 'roller-skate' ){
+    return `This Saturday, I want to ${defaultActivity}!`;
+}
+saturdayFun();
+
+function mondayWork(defaultActivity ='go to the office') 
+{
+    return `This Monday, I will ${defaultActivity}.`
+    
+}
+mondayWork();
+let wrapAdjective = function(arsterik="*") {
+    return function(adjective="special") {
+      return `You are ${arsterik}${adjective}${arsterik}!`
+    }
+  }
